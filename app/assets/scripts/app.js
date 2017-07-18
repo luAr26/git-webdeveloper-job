1f35f9df3cab341a86ff9303977c23f1959970d6
luuaAr26 <raul_cosmin_26@yahoo.com>
2017-07-18 23:21:26 +0300

    Completed the modal overlay

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,9 +2,10 @@ import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 new MobileMenu();
 new RevealOnScroll($('.featured-item'), '85%');
 new RevealOnScroll($('.testimonial'), '60%');
-
-const stickyHeader = new StickyHeader();
+new StickyHeader();
+new Modal();
---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,37 @@
+import $ from 'jquery';
+
+class Modal {
+  constructor() {
+    this.openModalButton = $('.open-modal');
+    this.modal = $('.modal');
+    this.closeModalButton = $('.modal__close');
+    console.log(this.openModalButton);
+    this.events();
+  }
+
+  events() {
+    // clicking the open modal button
+    this.openModalButton.on('click', this.openModal.bind(this));
+    // clicking the x close modal button
+    this.closeModalButton.on('click', this.closeModal.bind(this));
+    // pushes any key
+    $(document).on('keyup', this.keypressHandler.bind(this));
+  }
+
+  openModal(e) {
+    this.modal.addClass('modal--is-visible');
+    e.preventDefault();
+  }
+
+  closeModal() {
+    this.modal.removeClass('modal--is-visible');
+  }
+
+  keypressHandler(e) {
+    if (e.keyCode === 27) {
+      this.closeModal();
+    }
+  }
+}
+
+export default Modal;
